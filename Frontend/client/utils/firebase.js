53229998e51ece610e6d85412ec600af9bbba010
Rishabh Rawat <150123998+RishabhRawat12345@users.noreply.github.com>
2026-02-17 21:44:27 +0530

    Update firebase.js

--- Frontend/client/utils/firebase.js
+++ Frontend/client/utils/firebase.js
@@ -6,11 +6,11 @@ import {getAuth, GoogleAuthProvider} from "firebase/auth"
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIRE_KEY,
-  authDomain: "lms-5477b.firebaseapp.com",
-  projectId: "lms-5477b",
-  storageBucket: "lms-5477b.firebasestorage.app",
-  messagingSenderId: "10309403513",
-  appId: "1:10309403513:web:3b03c18b9b049f8d92a5a8"
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
